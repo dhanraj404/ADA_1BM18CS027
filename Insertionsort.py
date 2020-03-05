5d2c5a64693bfe037ad473146de9df98528c8b4b
dhanraj404 <53806611+dhanraj404@users.noreply.github.com>
2020-03-05 11:27:39 +0530

    Update Insertionsort.py

--- Insertionsort.py
+++ Insertionsort.py
@@ -1,1 +1,18 @@
-#insertionsort
+
+# Insertion Sort 
+
+def insertionSort(arr): 
+
+	for i in range(1, len(arr)): 
+
+		key = arr[i] 
+		j = i-1
+		while j >= 0 and key < arr[j] : 
+				arr[j + 1] = arr[j] 
+				j -= 1
+		arr[j + 1] = key
+	for i in arr:
+		print("%d"%i) 
+
+arr = list(map(int, input("Enter the Array elements:\n").split()))
+insertionSort(arr)
